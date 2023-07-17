792f1923f88a2b149dd3bdd7a65d811c3eac10ad
3bdalah.7asan <53155597+Abdullah24102000@users.noreply.github.com>
2023-07-18 00:53:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.pic')
+let container = document.querySelector('.container')
+
+function phones(phone){
+    img.src = phone;
+}
+
+function colors(color){
+    container.style.background = color;
+}
